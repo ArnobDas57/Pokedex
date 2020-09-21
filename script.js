7d419e23c750a172fcd087427c06a54821b0a882
Arnob Das <adas46@uwo.ca>
2020-09-21 16:02:56 -0400

    Created second input validation function and added javascript comments to explain logic

--- script.js
+++ script.js
@@ -41,3 +41,58 @@ function validateInput1()
         }
     }
 }
+
+//function that will validate input that should contain letters and no more than 20 characters
+function validateInput2()
+ {
+    var searchInput2 = document.getElementById("input2").value;
+    var ul = document.getElementById("poke-list");
+    var popup = document.getElementById("myPopup");
+    var li = ul.getElementsByTagName("li");
+    var c, text;
+
+    /* for loop iterates through each character of the second search box input,
+    if statement checks if each character is a letter, if the character's lowercase equals the characters uppercase that
+    means it is not a letter (for ex: 'a' does not equal to 'A')*/
+    for (c of searchInput2)
+    {
+        if(c.toLowerCase() === c.toUpperCase())
+        {
+            popup.style.display = "block";
+
+            text = "Input not valid - Please enter letters A-Z or a-z and no more than 20 characters";
+            document.getElementById("p1").innerHTML = text;
+        }
+    }
+
+    //if the search box input is empty or has more than 20 characters than popup with error text appears
+    if(searchInput2.length > 20 || searchInput2.length < 1)
+    {
+        popup.style.display = "block";
+
+        text = "Input not valid - Please enter letters A-Z or a-z and no more than 20 characters";
+        document.getElementById("p1").innerHTML = text;
+    }
+
+    //if there are no errors in the search box input then else statement will run
+    else 
+    {
+        popup.style.display = "block";
+
+        var counter = 0;
+
+       /* for loop iterates 20 times (length of li array), if the li array element at current iteration number includes the search input within the text then
+        the pokemon at that array position is added to the popup of results */
+        for(var i = 0; i < li.length; i++)
+        {
+            if((li[i].textContent).includes(searchInput2))
+            {
+                text = li[i].textContent;
+                document.getElementById("p" + (counter + 1)).innerHTML = text;
+                counter++;
+            }
+
+            if(counter === 5) break;
+        }
+    }
+}
